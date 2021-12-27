0fcb828018269e127aa0d06aba2d892dfee328d7
Dzhin2001 <Dzhin2001@mail.ru>
2021-12-27 17:19:43 +0300

    Декоративные элементы 2

--- source/js/menu-nav.js
+++ source/js/menu-nav.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   var navMain = document.querySelector('.main-nav__list');
   var navToggle = document.querySelector('.main-nav__toggle');
+  navMain.classList.remove('main-nav__list--no-js');
   navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav__list--hidden')) {
-      navMain.classList.remove('main-nav__list--hidden');
-    } else {
-      navMain.classList.add('main-nav__list--hidden');
-    }
+    navMain.classList.toggle('main-nav__list--closed');
+    navMain.classList.toggle('main-nav__list--open');
   });
 })
