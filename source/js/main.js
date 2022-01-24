51defa9e63c4266920b9bd085cd112990d489030
Dzhin2001 <Dzhin2001@mail.ru>
2022-01-24 21:09:16 +0300

    Декоративные элементы 11

--- source/js/main.js
+++ source/js/main.js
@@ -7,62 +7,70 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   // form
-  let modalToggle = function() {
-    document.querySelector('.modal-container').classList.toggle('modal-container--close');
-  }
-  document.getElementById('button-error').onclick = modalToggle;
-  document.getElementById('button-success').onclick = modalToggle;
+  let form = document.querySelector('.form__form');
+  if(typeof(form) != 'undefined' && form != null) {
+    let modalToggle = function() {
+      let modalContainer = document.querySelector('.modal-container');
+      modalContainer.classList.toggle('modal-container--close');
+    };
+    let buttonError = document.getElementById('button-error');
+    let buttonSuccess = document.getElementById('button-success');
+    buttonError.onclick = modalToggle;
+    buttonSuccess.onclick = modalToggle;
 
-  document.querySelector('.form__form').onsubmit = function() {
-    /* do what you want with the form */
-    let valid = true;
-    if(document.getElementById('input__surname').value.length == 0) {
-      valid = false;
-    }
-    if(document.getElementById('input__first-name').value.length == 0) {
-      valid = false;
-    }
-    let r = /^\w+@\w+\.\w{2,4}$/i;
-    if (!r.test(document.getElementById('input__email').value)) {
-      valid = false;
-    }
+    form.onsubmit = function() {
+      let windowModal = document.querySelector('.modal');
+      let inputSurname = document.getElementById('input__surname');
+      let inputFirstname = document.getElementById('input__first-name');
+      let inputEmail = document.getElementById('input__email');
+      let r = /^\w+@\w+\.\w{2,4}$/i;
+      let valid = true;
 
-    let windowModal = document.querySelector('.modal');
-    windowModal.classList.toggle('modal--success', valid);
-    modalToggle();
+      if( inputSurname.value.length == 0
+          || inputFirstname.value.length == 0
+          || !r.test(inputEmail.value)
+        ) {
+        valid = false;
+      }
+      windowModal.classList.toggle('modal--success', valid);
+      modalToggle();
 
-    // You must return false to prevent the default form behavior
-    return false;
-    //return valid;
+      // You must return false to prevent the default form behavior
+      return false;
+      //return valid;
+    }
   }
 
   // map
   let center = [59.938635, 30.323122];
   let navMain = document.querySelector('.contacts__map');
-  navMain.classList.remove('contacts__map--no-js');
+  if(typeof(navMain) != 'undefined' && navMain != null) {
+    navMain.classList.remove('contacts__map--no-js');
 
-  function init() {
-    let map = new ymaps.Map('map-contacts', {
-      center: center,
-      zoom: 16
-    });
-    map.geoObjects
-      .add(new ymaps.Placemark([59.938635, 30.323122], {
-        balloonContent: '<strong>html Academy</strong>',
-        iconCaption: ''
-      }, {
-        preset: 'islands#redCircleDotIconWithCaption',
-        iconCaptionMaxWidth: '50'
-      }));
-    map.controls.remove('geolocationControl'); // удаляем геолокацию
-    map.controls.remove('searchControl'); // удаляем поиск
-    map.controls.remove('trafficControl'); // удаляем контроль трафика
-    map.controls.remove('typeSelector'); // удаляем тип
-    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-    map.controls.remove('rulerControl'); // удаляем контрол правил
-    map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+    function init() {
+      let map = new ymaps.Map('map-contacts', {
+        center: center,
+        zoom: 16
+      });
+      map.geoObjects
+        .add(new ymaps.Placemark([59.938635, 30.323122], {
+          balloonContent: '<strong>html Academy</strong>',
+          iconCaption: ''
+        }, {
+          preset: 'islands#redCircleDotIconWithCaption',
+          iconCaptionMaxWidth: '50'
+        }));
+      map.controls.remove('geolocationControl'); // удаляем геолокацию
+      map.controls.remove('searchControl'); // удаляем поиск
+      map.controls.remove('trafficControl'); // удаляем контроль трафика
+      map.controls.remove('typeSelector'); // удаляем тип
+      map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+      map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+      map.controls.remove('rulerControl'); // удаляем контрол правил
+      map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+    }
+
+    ymaps.ready(init);
   }
 
-  ymaps.ready(init);
 })
